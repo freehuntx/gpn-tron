060076485de2f55816044b24f7579a318753f5f3
FreehuntX <freehuntx@web.de>
2023-06-09 02:04:54 +0200

    Decrease basetickrate again

--- shared/constants/common.ts
+++ shared/constants/common.ts
@@ -1,5 +1,5 @@
-export const baseTickrate = 3 // How many ticks per second?
-export const tickIncreaseInterval = 15 // How much time to wait until increasing the tickrate by 1
+export const baseTickrate = 1 // How many ticks per second?
+export const tickIncreaseInterval = 20 // How much time to wait until increasing the tickrate by 1
 export const afkTimeout = 0 // After what amount of ms not sending data a user is count as afk?
 export const joinTimeout = 5000 // After what amount of ms not sending the join packet should a user time out?
 //export const maxPacketsPerSecond = baseTickrate + 3 // How many packets per second are allowed?
